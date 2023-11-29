485d6919b3bd1ff970138ccd061a725cac6eba2a
GUDIPATI BHARGAVI <gudipatibhargavi182@gmail.com>
2023-11-29 13:26:10 +0530

    added folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MovieComponent } from './movie/movie.component';
+import { MovieComponent } from './movievoting/movie/movie.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MovieComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MovieComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
